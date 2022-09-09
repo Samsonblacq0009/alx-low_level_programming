326bd7ceee6cdae3939288acdf74f4fcdb427899
Samsonblacq0009 <samsonblacq0009@gmal.com>
2022-09-09 14:47:55 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int a, b;
-
+	
 	for (a = '0'; a <= '9'; a++)
 	{
 			for (b = '0'; b <= '9'; b++)
@@ -25,7 +25,6 @@ int main(void)
 						putchar(' ');
 					}
 				}
-			
 			}
 	}
 	putchar('\n');
