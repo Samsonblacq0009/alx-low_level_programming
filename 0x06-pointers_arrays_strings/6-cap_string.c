df7adbb8f0d142233381671f0a6a6546431ad8dd
Samsonblacq0009 <111820395+Samsonblacq0009@users.noreply.github.com>
2022-09-21 22:14:01 +0100

    Update file 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,28 @@
 #include "main.h"
 /**
- * cap_string - function that capitalizes all words of a string.
- * @s: string
- * Return: output
- */
-char *cap_string(char *s)
+* *cap_string - Capitalizes all words of a string
+*@c: string to change
+*Return: char changed
+*/
+char *cap_string(char *c)
 {
-	int i = 0;
-	if (s[i] >= 97 && s[i] <= 122)
-	{
-		s[i] -= 32;
-	}
-	while (s[i] != '\0')
-	{
-		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'
-			|| s[i] == ',' || s[i] == ';' || s[i] == '.'
-			|| s[i] == '!' || s[i] == '?' || s[i] == '"'
-			|| s[i] == '(' || s[i] == ')' || s[i] == '{'
-			|| s[i] == '}')
-		{
-			if (s[i + 1] >= 97 && s[i + 1] <= 122)
-			{
-				s[i + 1] -= 32;
-			}
-		}
-		i++;
-	}
-	return (s);
+int i;
+
+if (c[0] >= 'a' && c[0] <= 'z')
+{
+c[0] = c[0] - 32;
+}
+for (i = 1; c[i] != '\0'; i++)
+{
+if ((c[i - 1] == ' ' || c[i - 1] == '\t' || c[i - 1] == '\n'
+|| c[i - 1] == ','
+|| c[i - 1] == ';' || c[i - 1] == '.' || c[i - 1] == '!'
+|| c[i - 1] == '?' || c[i - 1] == '"' || c[i - 1] == '('
+|| c[i - 1] == ')' || c[i - 1] == '{' || c[i - 1] == '}')
+&& (c[i] > 'a' && c[i] < 'z'))
+{
+c[i] = c[i] - 32;
+}
+}
+return (c);
 }
