06878526d45a67ef6058d94841944549b6fb284d
Samsonblacq0009 <samsonblacq0009@gmail.com>
2022-09-26 14:31:28 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,6 +5,7 @@
  * @dest: destination to copy to
  * @src: source to copy from
  * @n: n amount of times
+ *
  * Return: return the string value
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-String readme
+Memcpy readme
